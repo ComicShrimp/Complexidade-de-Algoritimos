ec51b7100f9324a392f7b14f283eac4a24d7bb0c
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2020-12-03 15:58:09 -0300

    feat(Intervalos Disjuntos): Adiciona Script
    Adiciona Script inicial para intervalos disjuntos

--- intervalosDisjuntos.py
+++ intervalosDisjuntos.py
@@ -1,1 +1,19 @@
-print([[5] * 5] * 5)
+def intervaloDisjunto(
+    inicio: list, fim: list, intervalo_inicio: int, intervalo_fim: int
+):
+    x = [0]
+    k = intervalo_inicio
+
+    for indice in range(1, intervalo_fim):
+        if inicio[indice] > fim[k]:
+            x.append(indice)
+            k = indice
+    return x
+
+
+a = [6, 9, 7, 18, 1, 23, 25, 30]
+b = [15, 15, 16, 24, 26, 28, 30, 34]
+p = 1
+r = 8
+
+print(intervaloDisjunto(a, b, p, r))
